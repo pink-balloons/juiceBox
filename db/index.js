9663c7674dad04f9569159d28a2eb7d2c5ada80c
thatsme <ambergray930@yahoo.com>
2021-10-27 18:13:32 -0400

    added

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const { Client } = require("pg"); // imports the pg module
 
 const client = new Client({
   connectionString:
-    process.env.DATABASE_URL || "postgres://postgres@/juicebox-dev",
+    process.env.DATABASE_URL || "postgres://localhost:5432/juicebox-dev",
   ssl:
     process.env.NODE_ENV === "production"
       ? { rejectUnauthorized: false }
